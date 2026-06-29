6410d27b931f0f24278d2a35fa6543cf96684070
Sage-NBA <sammy2007bright@gmail.com>
2026-06-29 18:58:56 +0100

    Add default home route

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import {Home} from "./component";
 import {Routes, Route} from "react-router-dom";
 import About from "./app/About";
 import Services from "./app/Services";
-import Contact from "./app/Contact"
+import Contact from "./app/Contact";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Services" element={<Services />} />
-        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </>
   );
