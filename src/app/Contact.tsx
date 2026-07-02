5a1769324c4d2fa7ca81b995baad5635f7efabba
Sage-NBA <sammy2007bright@gmail.com>
2026-07-02 10:20:25 +0100

    made some new changes

--- src/app/Contact.tsx
+++ src/app/Contact.tsx
@@ -1,38 +1,32 @@
 function Contact() {
   return (
     <div className="  p-5">
-      <div className="md:flex justify-between itens-center">
-        <p className="text-4xl md:text-7xl wrap-break-word w-80 md:w-130">
+      <div className="md:flex py-5 gap-10 md:py-10 md:my-10 md:ml-10 space-y-5">
+        <p className="text-3xl sm:text-4xl break-word w-80 md:w-70">
           Let's Connect Out There
         </p>
-        <p className="hidden md:flex w-screen md:w-50 h-20">
-          <img
-            className="w-full h-full rounded-3xl object-center"
-            src="/images/mark.jpeg"
-          ></img>
-        </p>
+        <div className="text-[20px] space-y-5 xl:flex lg:gap-20">
+          <p className="flex gap-2 md:gap-10 lg:gap-5">
+            <i className="ri-mail-fill"></i>
+            <p>Sammy2007bright@gmail.com</p>
+          </p>
+          <p className="flex gap-2 md:gap-10 lg:gap-3">
+            <i className="ri-phone-fill"></i>
+            <p>+234 9070685246</p>
+          </p>
+          <p className="flex gap-2 md:gap-10 lg:gap-3">
+            <i className="ri-map-pin-fill"></i>
+            <p>Lagos</p>
+          </p>
+        </div>
       </div>
       {/* another */}
-      <p className="text-center text-4xl p-5">
-        Have an Idea? Let's Make it Happen
-      </p>
-      <div className="text-[20px] md:text-2xl">
-        <p className="flex gap-2 md:gap-10">
-          <i className="ri-mail-fill"></i>
-          <p>Sammy2007bright@gmail.com</p>
-        </p>
-        <p className="flex gap-2 md:gap-10">
-          <i className="ri-phone-fill"></i>
-          <p>+234 9070685246</p>
-        </p>
-        <p className="flex gap-2 md:gap-10">
-          <i className="ri-map-pin-fill"></i>
-          <p>Lagos</p>
-        </p>
-      </div>
+
       {/* socials */}
-      <p className="text-center text-4xl p-5">Connect Wth Me On!!</p>
-      <div className="grid grid-cols-2 md:gap-20 justify-center text-center space-y-5 py-5 md:flex text-7xl list-none">
+      <p className="text-center text-2xl md:text-3xl p-5">
+        Connect Wth Me On!!
+      </p>
+      <div className="grid grid-cols-2 md:gap-20 text-center justify-center space-y-5 py-5 md:flex text-4xl md:text-5xl list-none">
         <a href="https://www.facebook.com/share/1EXC4sJ43C/">
           <li>
             <i className="ri-facebook-circle-fill"></i>
@@ -54,16 +48,20 @@ function Contact() {
           </li>
         </a>
       </div>
-      <section className="text-white text-2xl p-5 space-y-5 rounded-2xl bg-[#423f3f] w-full h-fit">
-        <h1 className="text-4xl text-center">Send A Message Today!!!!</h1>
-        <p>* Frontend Developer</p>
-        <p>* Remote Friendly</p>
-        <p>* Open for Freelance Projects</p>
-        <p>* Available for Internships</p>
-        <p style={{fontFamily: "Bitcount Grid Double"}} className="text-end">
-          Responds within 24hrs
-        </p>
-      </section>
+      <div className="bg-[gray] p-5 md:p-10 rounded-2xl space-y-5 md:space-y-10">
+        <h1 className="text-2xl md:text-4xl text-center">
+          Send A Message Today!!!!
+        </h1>
+        <div className=" space-y-5">
+          <p>* Frontend Developer</p>
+          <p>* Remote Friendly</p>
+          <p>* Open for Freelance Projects</p>
+          <p>* Available for Internships</p>
+          <p style={{fontFamily: "Bitcount Grid Double"}} className="text-end">
+            Responds within 24hrs
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
